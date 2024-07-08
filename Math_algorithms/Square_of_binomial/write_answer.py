ba07ade2bedd098f8d5b0553bfa0a6aec41bc278
k1time <k1time@mail.ru>
2024-07-09 00:09:19 +0300

    Create write_answer.py

---
+++ Math_algorithms/Square_of_binomial/write_answer.py
@@ -0,0 +1,9 @@
+# calling the method of allocating the square of a binomial
+
+from square import complete_the_square
+
+a = int(input("a? "))
+b = int(input("b? "))
+c = int(input("c? "))
+
+print(f"Answer: {complete_the_square(a, b, c)}")
