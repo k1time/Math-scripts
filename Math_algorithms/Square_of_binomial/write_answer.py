7c872b11c6e7570af5317cb025e92b1783f8f977
k1time <k1time@mail.ru>
2024-09-22 23:34:09 +0300

    Delete Math_algorithms/Square_of_binomial directory

--- Math_algorithms/Square_of_binomial/write_answer.py
+++
@@ -1,9 +0,0 @@
-# calling the method of allocating the square of a binomial
-
-from square import complete_the_square
-
-a = int(input("a? "))
-b = int(input("b? "))
-c = int(input("c? "))
-
-print(f"Answer: {complete_the_square(a, b, c)}")
